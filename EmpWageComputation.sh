7ba2256a1241edbb29fc0f7c4b06d06ec6bc7f17
nareshsahu0 <nareshsahu158@gmail.com>
2023-01-06 17:20:04 +0530

    Assignment

---
+++ EmpWageComputation.sh
@@ -0,0 +1,27 @@
+echo "Welcome to Employee Wage Computation Program"
+
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+totalSalary=0
+numWorkingDays=20
+for (( day1; day<=$numWorkingDays; day++))
+do
+empCheck=$((RANDOM%3));
+case $empCheck in
+$isFullTime)
+echo "Employe is full-time present"
+empHrs=8
+;;
+$isPartTime)
+echo "Employee is part-time present"
+empHrs=4
+;;
+*)
+echo "Employee is absent"
+empHrs=0
+;;
+esac
+salary=$(($empHrs*$empRatePerHr))
+totalSalary=$(($totalSalary+$salary))
+done
